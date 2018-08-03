b554a69b9f136bacb7778b6d394fd7e7a1b67f6d
LidiaSousaf <lidia_sousaf@hotmail.com>
2018-08-03 15:04:30 +0100

    Adicionados roles

---
+++ RentYourCar_PWEB/Models/RoleNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentYourCar_PWEB.Models
+{
+    public static class RoleNames
+    {
+        public const string Admin = "Admin";
+        public const string Profissional = "Profissional";
+        public const string Particular = "Particular";
+    }
+}
--- RentYourCar_PWEB/Startup.cs
+++ RentYourCar_PWEB/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using RentYourCar_PWEB.Models;
 
 [assembly: OwinStartupAttribute(typeof(RentYourCar_PWEB.Startup))]
 namespace RentYourCar_PWEB
@@ -9,6 +12,66 @@ namespace RentYourCar_PWEB
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+        }
+
+        private void CreateRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            //Criar Role de "Admin" e um utilizador para este papel
+            if (!roleManager.RoleExists(RoleNames.Admin))
+            {
+                //Criar o Role "Admin"
+                var role = new IdentityRole
+                {
+                    Name = RoleNames.Admin
+                };
+                roleManager.Create(role);
+
+                //Criar o utilizador Admin
+                var user = new ApplicationUser
+                {
+                    UserName = "Admin",
+                    Email = "admin@rentyourcar.com"
+                };
+
+                string password = "_Password123";
+
+                var createResult = userManager.Create(user, password);
+
+                //Adicionar o Role "Admin" ao utilizador por defeito
+                if (createResult.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, RoleNames.Admin);
+
+                }
+            }
+
+            //Criar o Role "Profissional"
+            if (!roleManager.RoleExists(RoleNames.Profissional))
+            {
+                var role = new IdentityRole
+                {
+                    Name = RoleNames.Profissional
+                };
+                roleManager.Create(role);
+
+            }
+
+            //Criar o Role "Particular"   
+            if (!roleManager.RoleExists(RoleNames.Particular))
+            {
+                var role = new IdentityRole
+                {
+                    Name = RoleNames.Particular
+                };
+                roleManager.Create(role);
+
+            }
         }
     }
 }
