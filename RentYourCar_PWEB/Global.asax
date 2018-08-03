bbb654591004b023c8f8e1cc3cb00980ad941d53
João Leitão <a21250715@isec.pt>
2018-08-03 12:29:23 +0100

    Add project files.

---
+++ RentYourCar_PWEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RentYourCar_PWEB.MvcApplication" Language="C#" %>
---
+++ RentYourCar_PWEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentYourCar_PWEB.Startup))]
+namespace RentYourCar_PWEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
